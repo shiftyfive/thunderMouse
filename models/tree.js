7109b2739d47c118f94eb4687be020e5fb0c3beb
Joshua <joshuawarren000@gmail.coml>
2017-07-01 21:06:31 -0700

    very basics of tree implementations added

--- models/tree.js
+++ models/tree.js
@@ -1,14 +1,17 @@
 'use strict'
 
 class Node {
-  this.data = data;
-  this.parent = null
-;
-this.children = [];}
+  constructor(tag, data) {
+    this.tag = tag
+    this.data = data || null
+    this.children = []
+    this.parent = null;
+  }
+}
 
 class Tree {
-  constructor(data){
-  var node = new Node(data);
+  constructor(tag, data){
+  var node = new Node(tag, data);
   this._root = node;
   }
 
@@ -21,4 +24,29 @@ class Tree {
       callback(currentNode);
     })(this._root);
   }
+
+  addFromJSON(tag, data, toData, traversal) {
+    var child = new Node(data)
+    var parent = null
+    var callback =function(node) {
+      if (node.data === toData) {
+        parent = node;
+      }
+    };
+    this.contains(callback, traversal);
+    if (parent) {
+      parent.children.push(child);
+      child.parent = parent;
+    } else {
+      throw new Error('Cannot add node to a non-existent parent.')
+    }
+  }
 }
+
+var tree = new Tree('<body>')
+
+tree._root.children.push(new Node('<section>'))
+
+tree.traverseDF(function(node) {
+  console.log(node.tag);
+})
