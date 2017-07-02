af74526076dccf3eff95c9c8bf3e05322943b64b
Joshua <joshuawarren000@gmail.coml>
2017-07-01 19:08:01 -0700

    removed file to prevent merge conflict with other branch

--- models/tree.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-class Tree {
-
-}
-
-class Node {
-  
-
-}
--- models/write.js
+++ models/write.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function append() {
   while(node !== head) {
     str.append(lastindexOf(`<${node.parent.tag}/>`)
