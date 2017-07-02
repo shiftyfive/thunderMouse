764d5b921d68ee5f2c4b2864b630fabcd5c71469
Joshua <joshuawarren000@gmail.coml>
2017-07-01 19:38:22 -0700

    added depth first search

--- models/tree.js
+++ models/tree.js
@@ -1,9 +1,24 @@
 'use strict'
-class Tree {
-
-}
 
 class Node {
+  this.data = data;
+  this.parent = null
+;
+this.children = [];}
 
+class Tree {
+  constructor(data){
+  var node = new Node(data);
+  this._root = node;
+  }
 
+  traverseDF(callback) {
+    (function recurse(currentNode) {
+      for (var i = 0; i < currentNode.children.length; i++) {
+        recurse(currentNode.children[i])
+      }
+
+      callback(currentNode);
+    })(this._root);
+  }
 }
